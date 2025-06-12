0953126ed00c0235398df4e88686a7a7d6ccb3a1
VoicedMist2 <estebancrespo2001@gmail.com>
2025-06-12 15:16:54 -0500

    Se crea la clase Series_Taylor y se arreglan problemas de identacion

--- Modelo/funciones.py
+++ Modelo/funciones.py
@@ -1,79 +1,80 @@
-# Calcula x elevado a la n veces
+class Series_Taylor:
 
-def elevado(x, n):
-    
-    resultado = 1
+    # Calcula x elevado a la n veces
+    def elevado(x, n):
+        
+        resultado = 1
 
-    for _ in range(n):
-        resultado *= x
-    return resultado
+        for _ in range(n):
+            resultado *= x
+        return resultado
 
-# Calcula el factorial de n
+    # Calcula el factorial de n
 
-def factorial(n):   
-      
-    if n == 0 or n == 1:
-        return 1
-    resultado = 1
-    
-    for i in range(1, n + 1):
-        resultado *= i
-    return resultado
+    def factorial(n):   
+        
+        if n == 0 or n == 1:
+            return 1
+        resultado = 1
+        
+        for i in range(1, n + 1):
+            resultado *= i
+        return resultado
 
-#Calcula e ** x
+    #Calcula e ** x
 
-def calcular_e_x(x, n):
-    resultado = 1
-    for i in range(1, n + 1):
-        resultado += elevado(x, i) / factorial(i)
-    return resultado
+    def calcular_e_x(x, n):
+        resultado = 1
+        for i in range(1, n + 1):
+            resultado += Series_Taylor.elevado(x, i) / Series_Taylor.factorial(i)
+        return resultado
 
-# Calcula sen x
+    # Calcula sen x
 
-def sen(x, n):
-    resultado = 0
-    for i in range(n):
-        resultado += elevado(-1, i) * elevado(x, 2*i + 1) / factorial(2*i + 1)
-    return resultado
+    def sen(x, n):
+        resultado = 0
+        for i in range(n):
+            resultado += Series_Taylor.elevado(-1, i) * Series_Taylor.elevado(x, 2*i + 1) / Series_Taylor.factorial(2*i + 1)
+        return resultado
 
-# Calcula cos x
+    # Calcula cos x
 
-def cos(x, n):
-    resultado = 0
-    for i in range(n):
-        resultado += elevado(-1, i) * elevado(x, 2 * i) / factorial(2 * i)
-    return resultado
+    def cos(x, n):
+        resultado = 0
+        for i in range(n):
+            resultado += Series_Taylor.elevado(-1, i) * Series_Taylor.elevado(x, 2 * i) / Series_Taylor.factorial(2 * i)
+        return resultado
 
-# Calcula arcsen x solo para x en el intervalo [-1, 1]
+    # Calcula arcsen x solo para x en el intervalo [-1, 1]
 
-def arcsen(x, n):
-    resultado = 0
-    for i in range(n):
-        resultado += factorial(2 * i)* elevado(x, 2 * i + 1)/ (elevado(4, i) * elevado(factorial(i), 2) * (2 * i + 1))
-    return resultado
+    def arcsen(x, n):
+        resultado = 0
+        for i in range(n):
+            resultado += Series_Taylor.factorial(2 * i)* Series_Taylor.elevado(x, 2 * i + 1)/ (Series_Taylor.elevado(4, i) * Series_Taylor.elevado(Series_Taylor.factorial(i), 2) * (2 * i + 1))
+        return resultado
 
-# Calcula arccos x solo para x en el intervalo [-1, 1]
-def arccos(x, n):
-    
-    resultado = 0
-    pi = 3.141592653589793
+    # Calcula arccos x solo para x en el intervalo [-1, 1]
+    def arccos(x, n):
+        
+        resultado = 0
+        pi = 3.141592653589793
 
-    resultado += (pi/2) - arcsen(x, n)
-    return resultado
+        resultado += (pi/2) - Series_Taylor.arcsen(x, n)
+        return resultado
 
-# Calcula senh x
+    # Calcula senh x
 
-def senh(x, n):
-    resultado = 0
-    
-    for i in range(n):
-        resultado += elevado(x, 2*i + 1) /factorial(2*i + 1)
-    return resultado
+    def senh(x, n):
+        resultado = 0
+        
+        for i in range(n):
+            resultado += Series_Taylor.elevado(x, 2*i + 1) / Series_Taylor.factorial(2*i + 1)
+        return resultado
 
-# Calcula cosh x
-def cosh(x, n):
-    resultado = 0
-    
-    for i in range(n):
-        resultado += elevado(x, 2 * i) / factorial(2 * i)
-    return resultado
+    # Calcula cosh x
+    def cosh(x, n):
+        resultado = 0
+        
+        for i in range(n):
+            resultado += Series_Taylor.elevado(x, 2 * i) / Series_Taylor.factorial(2 * i)
+        return resultado
